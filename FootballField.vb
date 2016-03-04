83422f08588b239a014bd0c9fb1123db66f4e005
Rick Wight <fmwight@wpi.edu>
2016-03-03 19:46:08 -0500

    created FootballField class with a 120x53 array of integers representing the field

---
+++ FootballField.vb
@@ -0,0 +1,18 @@
+﻿
+Option Explicit On
+Option Strict On
+
+Imports System
+
+
+
+Public Class FootballField
+	'initializing field as an integer array as a placeholder
+	'could be changed to hold players, strings, whatever
+	dim field(120,53) As Integer
+	
+	Public Sub New()
+		MyBase.New
+	End Sub
+End Class
+
