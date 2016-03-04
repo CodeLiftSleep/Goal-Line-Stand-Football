c1b77902cfe57ff8adfaa5decf9b2308e784ba3e
Rick Wight <fmwight@wpi.edu>
2016-03-03 19:51:30 -0500

    Added comments to FootballField

--- FootballField.vb
+++ FootballField.vb
@@ -5,10 +5,11 @@ Option Strict On
 Imports System
 
 
-
+' a class containing an array of [blanks] representing the football field, and methods for accessing, 
+' manipulating, and identifying that array.
 Public Class FootballField
-	'initializing field as an integer array as a placeholder
-	'could be changed to hold players, strings, whatever
+	' initializing field as an integer array as a placeholder
+	' could be changed to hold players, strings, whatever
 	dim field(120,53) As Integer
 	
 	Public Sub New()
