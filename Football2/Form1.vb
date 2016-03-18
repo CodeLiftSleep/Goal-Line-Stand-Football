afa67a6dda2ed2348f5547eb239ec471bff42e8a
Matt Erman <matter2003@yahoo.com>
2016-03-18 09:35:48 -0400

    Updating with New League Calendar Implemented

--- Football2/Form1.vb
+++ Football2/Form1.vb
@@ -1,23 +1,38 @@
 Imports System.Data.SQLite
-'Imports System.Text
+Imports System.IO
+Imports Troschuetz.Random.Generators.XorShift128Generator
+Imports LeagueCalendar.LeagueState
 Public Class Form1
 
     Dim SQL As SQLFunctions.SQLiteDataFunctions = New SQLFunctions.SQLiteDataFunctions
+    Dim MyRand As New Troschuetz.Random.TRandom
+    Dim Getnum As Double
+    Dim GetGaus As Double
+    Public GetDay As New LeagueCalendar.LeagueCalendar
+    Public MyDate As Date
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         People.LoadData()
         Randomize()
-        'Dim SQLFieldNames = "Number varchar(5), Flt varchar(5), Name varchar(5)"
+
+        For i As Integer = 1 To 20 'properly initializes the random number generator
+            Getnum = MyRand.NextUInt(0, 100)
+            GetGaus = CInt(MyRand.Normal(49.5, 16.5))
+        Next i
+        For i As Integer = 1 To 365 'testing out the LeagueCalendar and RaiseEvent code
+            MyDate = GetDay.AdvDay(GetDay.CurDate, 1).ToShortDateString()
+            Console.WriteLine("Date: {0}", MyDate.ToShortDateString())
+        Next i
+
+
         Try
             CollegePlayer.GenDraftPlayers(3000)
-            NFLScout.GenScouts(800)
-            NFLOwner.GenOwners(100)
-            NFLGM.GenGMs(100)
-            NFLCoach.GenCoaches(800)
-            NFLPlayer.GetRosterPlayers(2600)
+            'NFLScout.GenScouts(800)
+            'NFLOwner.GenOwners(100)
+            'NFLGM.GenGMs(100)
+            'NFLCoach.GenCoaches(800)
+            'NFLPlayer.GetRosterPlayers(2600)
         Catch ex As System.ArgumentException
-            'Console.WriteLine(ex.GetBaseException)
-            'Console.WriteLine(ex.Data)
             Console.WriteLine(ex.ToString)
             Console.WriteLine(ex.Message)
         End Try
