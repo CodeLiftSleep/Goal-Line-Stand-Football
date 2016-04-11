0e7b09b09e5a0547d4285331c30e4aa02b4c0970
Matt Erman <matter2003@yahoo.com>
2016-04-11 01:05:40 -0400

    All Generation Code Is Now active
    Includes Personalities based on Raymond Cattell's 16PF, Agents that now have a list of clients based on how popular they are, players are now represented by an agent, trainers, etc...

--- Football2/Form1.vb
+++ Football2/Form1.vb
@@ -4,7 +4,6 @@ Imports Troschuetz.Random.Generators.XorShift128Generator
 Imports LeagueCalendar.LeagueState
 Public Class Form1
 
-    Dim SQL As SQLFunctions.SQLiteDataFunctions = New SQLFunctions.SQLiteDataFunctions
     Dim MyRand As New Troschuetz.Random.TRandom
     Dim Getnum As Double
     Dim GetGaus As Double
@@ -19,19 +18,19 @@ Public Class Form1
             Getnum = MyRand.NextUInt(0, 100)
             GetGaus = CInt(MyRand.Normal(49.5, 16.5))
         Next i
-        For i As Integer = 1 To 365 'testing out the LeagueCalendar and RaiseEvent code
-            MyDate = GetDay.AdvDay(GetDay.CurDate, 1).ToShortDateString()
-            Console.WriteLine("Date: {0}", MyDate.ToShortDateString())
-        Next i
-
-
+        ' For i As Integer = 1 To 365 'testing out the LeagueCalendar and RaiseEvent code
+        'MyDate = GetDay.AdvDay(GetDay.CurDate, 1).ToShortDateString()
+        'Console.WriteLine("Date: {0}", MyDate.ToShortDateString())
+        'Next i
         Try
-            CollegePlayer.GenDraftPlayers(3000)
-            'NFLScout.GenScouts(800)
-            'NFLOwner.GenOwners(100)
-            'NFLGM.GenPersonnel(850)
-            'NFLCoach.GenCoaches(1300)
-            'NFLPlayer.GetRosterPlayers(2600)
+            GeneratePeople("NFLPlayer", 2000) 'NFL Players have to be generated prior to agents, as they use the player DB
+            GeneratePeople("Coach", 1500)
+            GeneratePeople("Agent", 714)
+            GeneratePeople("CollegePlayer", 3800)
+            GeneratePeople("Owner", 100)
+            GeneratePeople("Personnel", 1500)
+            GeneratePeople("Trainer", 400)
+
         Catch ex As System.ArgumentException
             Console.WriteLine(ex.ToString)
             Console.WriteLine(ex.Message)
--- Football2/GlobalVar.vb
+++ Football2/GlobalVar.vb
@@ -3,97 +3,105 @@
 ''' </summary>
 Module GlobalVar
     'Module contains Global Variables for various functions
-    Public GetSQLite As New SQLFunctions.SQLiteDataFunctions
-
-    'Public GetTables As New SQLFunctions.DataFunctions
-    'Public GetPBP As New PBP
-    'Public PBPDT As New DataTable
-    'Public OwnerDT As New DataTable
-    'Public PlayerDT As New DataTable
-    'Public DraftDT As New DataTable
-    'Public GMDT As New DataTable
-    'Public CoachDT As New DataTable
-    'Public TrainerDT As New DataTable
-    'Public ScoutDT As New DataTable
-    'Public CityInfo As New DataTable
-    Public HomeTeamDT As New DataTable
-    Public AwayTeamDT As New DataTable
-    'Public FBall As New DataSet
-    'Public PlayStats As New DataTable
-    'Public PlayData As New DataTable
     Public MT As New Mersenne.MersenneTwister
-    Public GenData As New Generation.Person
-    'Public ScoutGradeDT As New DataTable
-    'Public TeamDraft As New Drafting
-    'Public TeamDraftDT As New DataTable
-    'Public WithEvents Game As New GameEngine
-    'Public WithEvents GetDec As New AIDecisionsInGame
-    'Public WithEvents GameClock As New Stopwatch
-    'Public WithEvents PlayClock As New Stopwatch
-    Public NFLPlayer As New Generation.NFLPlayers
-    Public NFLCoach As New Generation.Coaches
-    Public NFLGM As New Generation.GeneralManager
-    Public NFLScout As New Generation.Scouts
-    Public NFLOwner As New Generation.Owner
-    Public CollegePlayer As New Generation.CollegePlayers
+    Dim OwnerDT As New DataTable
+    Dim DraftDT As New DataTable
+    Dim AgentDT As New DataTable
+    Dim CoachDT As New DataTable
+    Dim TrainerDT As New DataTable
+    Dim PersonnelDT As New DataTable
+    Dim PlayerDT As New DataTable
+
+    Dim NFLPlayer As New Generation.NFLPlayers
+    Dim NFLAgent As New Generation.Agent
+    Dim NFLPersonnel As New Generation.Personnel
+    Dim NFLOwner As New Generation.Owner
+    Dim CollegePlayer As New Generation.CollegePlayers
     Public People As New Generation.Person
-    Public GenSchedule As New NFLScheduler.Scheduler
+    Dim NFLCoach As New Generation.Coaches
+    Dim NFLTrainer As New Generation.Trainer
+    Dim MyDraftClass As New ArrayList
+    Dim MyDB As String = "Football"
 
-    'Public Sub GetDS() 'Adds tables to the dataset
-    'FBall.Tables.Add(PBPDT)
-    'FBall.Tables.Add(OwnerDT)
-    'FBall.Tables.Add(PlayerDT)
-    'FBall.Tables.Add(GMDT)
-    'FBall.Tables.Add(DraftDT)
-    'FBall.Tables.Add(TrainerDT)
-    'FBall.Tables.Add(ScoutDT)
-    'FBall.Tables.Add(CoachDT)
-    'FBall.Tables.Add(PlayStats)
-    'FBall.Tables.Add(PlayData)
-    'FBall.Tables.Add(ScoutGradeDT)
-    'FBall.Tables.Add(TeamDraftDT)
-    'FBall.Tables.Add(CityInfo)
-    'FBall.Tables.Add(HomeTeamDT)
-    'FBall.Tables.Add(AwayTeamDT)
+    ''' <summary>
+    ''' This is meant to run once at the onset of the game---creates all the initial people involved in the game
+    ''' </summary>
+    ''' <param name="PersonType"></param>
+    ''' <param name="NumPeople"></param>
+    Public Sub GeneratePeople(ByVal PersonType As String, ByVal NumPeople As Integer)
+        Try
+            Select Case PersonType
 
+                Case "Coach"
+                    NFLCoach.Initialize(MyDB, CoachDT, "Coaches", NFLCoach.GetSQLString("Coach"))
+                    For i As Integer = 1 To NumPeople
+                        NFLCoach.GenCoaches(i, NFLCoach, CoachDT)
+                    Next i
+                    NFLCoach.PutCoachesOnTeams(CoachDT)
+                    CoachDT.Rows.RemoveAt(0)
+                    NFLCoach.Update(MyDB, CoachDT, "Coaches")
 
-    Public Sub Instantiate()
+                Case "CollegePlayer"
+                    CollegePlayer.Initialize(MyDB, DraftDT, "DraftPlayers", CollegePlayer.GetSQLString("College"))
+                    MyDraftClass = CollegePlayer.GenDraftClass(MyDraftClass)
+                    For i As Integer = 1 To NumPeople
+                        CollegePlayer.GenDraftPlayers(i, CollegePlayer, DraftDT, MyDraftClass)
+                    Next i
+                    DraftDT.Rows.RemoveAt(0)
+                    CollegePlayer.Update(MyDB, DraftDT, "DraftPlayers")
 
-        'Catch ex As Exception
-        'Console.WriteLine(ex.Message)
-        'End Try
-        'GetDS()
-        'People.LoadData()
-        'Try
-        'NFLPlayer.GetRosterPlayers(2555) ''###TODO Figure out why it is skipping rows at some places...
-        'CollegePlayer.GenDraftPlayers(2555)
-        'NFLCoach.GenCoaches(800)
-        'NFLGM.GenGMs(96)
-        'NFLScout.GenScouts(800)
-        'NFLOwner.GenOwners(96)
-        'Catch ex As System.InvalidCastException
-        'System.Data.SqlClient.SqlException
-        'Console.WriteLine(ex.Message)
-        'End Try
-        'GenSchedule.GetSchedule(16)
-        'GenData.GetRosterPlayers(2600)
-        'GenData.GenOwners(96)
-        'GenData.GenGMs(96)
-        'GenData.GenCoaches(800)
-        'GenData.GenScouts(800)
-        'GenData.GenDraftPlayers(2555)
-        'GenData.GenDraftClass(2555)
-        'Game.InGame()
-        'Eval.ScoutPlayerEval()
-        'GenData.GenScoutGrades(800, 2555)
-        'GetTables.DeleteTable(PBPDT, "PBP")
-        'GetTables.LoadTable(PBPDT, "PBP")
-        'GetPBP.GenSQL()
-        'GetPBP.GetPBP("pbptest.txt")
-        'GetPBP.GetPBP("fix2002pbp.txt")
-        'GetPBP.GetPBP("2003pbp.txt")
-        'GetTables.UpdateTable(PBPDT, "PBP")
-        'GetSQL.StoredProcs()
+                Case "NFLPlayer"
+                    NFLPlayer.Initialize(MyDB, PlayerDT, "RosterPlayers", NFLPlayer.GetSQLString("NFL"))
+                    For i As Integer = 1 To NumPeople
+                        NFLPlayer.GetRosterPlayers(i, NFLPlayer, PlayerDT)
+                    Next i
+                    For i As Integer = 1 To 17
+                        NFLPlayer.PutPlayerOnTeam(i, PlayerDT)
+                    Next i
+
+                Case "Personnel"
+                    NFLPersonnel.Initialize(MyDB, PersonnelDT, "Personnel", NFLPersonnel.GetSQLString("Personnel"))
+                    For i As Integer = 1 To NumPeople
+                        NFLPersonnel.GenPersonnel(i, NFLPersonnel, PersonnelDT)
+                    Next i
+                    PersonnelDT.Rows.RemoveAt(0)
+                    NFLPersonnel.Update(MyDB, PersonnelDT, "Personnel")
+
+                Case "Owner"
+                    NFLOwner.Initialize(MyDB, OwnerDT, "Owners", NFLOwner.OwnerSQLString)
+                    For i As Integer = 1 To NumPeople
+                        NFLOwner.GenOwners(i, NFLOwner, OwnerDT)
+                    Next i
+                    NFLOwner.GetTeamOwner(OwnerDT)
+                    OwnerDT.Rows.RemoveAt(0)
+                    NFLOwner.Update(MyDB, OwnerDT, "Owners")
+
+                Case "Agent"
+                    NFLAgent.Initialize(MyDB, AgentDT, "Agents", NFLAgent.AgentSQLString)
+                    For i As Integer = 1 To NumPeople
+                        NFLAgent.GenAgents(i, NFLAgent, AgentDT, PlayerDT)
+                    Next i
+                    AgentDT.Rows.RemoveAt(0)
+                    PlayerDT.Rows.RemoveAt(0)
+                    NFLAgent.Update(MyDB, AgentDT, "Agents")
+                    NFLPlayer.Update(MyDB, PlayerDT, "RosterPlayers")
+
+                Case "Trainer"
+                    NFLTrainer.Initialize(MyDB, TrainerDT, "Trainers", NFLTrainer.TrainerSQLString)
+                    For i As Integer = 1 To NumPeople
+                        NFLTrainer.GenTrainers(i, NFLTrainer, TrainerDT)
+                    Next i
+                    NFLTrainer.PutTrainerOnTeam(TrainerDT)
+                    TrainerDT.Rows.RemoveAt(0)
+                    NFLTrainer.Update(MyDB, TrainerDT, "Trainers")
+            End Select
+        Catch ex As System.Invalidcastexception
+            Console.WriteLine(Ex.Message)
+            Console.WriteLine(ex.Data)
+        End Try
     End Sub
+
+
+
 
 End Module
